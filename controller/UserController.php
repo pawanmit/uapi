839a4859b3b981da2f052a22b77396d7b6316563
pawanmit <mitpawan@gmail.com>
2014-03-07 14:46:55 -0800

    creating model from user request

--- controller/UserController.php
+++ controller/UserController.php
@@ -4,48 +4,64 @@ require_once '../model/User.php';
 
 class UserController {
 
-    public function post() {
-        $user = new User();
-        $user->last_name['value'] = 'carson';
-        $user->first_name['value'] = 'timmy';
-        $user->login['value'] = "tcarson";
-        $user->inputFields = array('last_name', 'first_name', 'login');
-        $user->create();
+    public $error = "";
+    private $model;
+
+    function __construct() {
+        $this->model = new User();
     }
 
-    public function put() {
-        $user = new User();
-        $user->last_name['value'] = 'Mittal';
-        $user->first_name['value'] = 'timmy';
-        //$user->login['value'] = "anamikaa";
-        $user->inputFields = array('last_name');
-        $user->filterFields = array('first_name');
-        $user->update();
+    public function post() {
+        $model = new User();
+        $model->last_name['value'] = 'carson';
+        $model->first_name['value'] = 'timmy';
+        $model->login['value'] = "tcarson";
+        $model->inputFields = array('last_name', 'first_name', 'login');
+        $model->create();
     }
+
 
     public function get() {
-        $user = new User();
-        $user->selectFields = array('last_name', 'login');
-        $user->first_name['value'] = 'timmy';
-        $user->filterFields = array('first_name');
-        $result = $user->find();
-        $users = self::normalizeOutput($result);
-        return (json_encode($users));
-        //return(($result));
+        $this->updateModelFromUserInput();
+        //print_r($this->model->filterFields);
+        $result = $this->model->find();
+        $models = self::normalizeOutput($result);
+        return $models;
     }
 
     private static function normalizeOutput($result) {
-        $users = array();
+        $models = array();
         $output = array();
         foreach($result as $row) {
-            $user = array();
+            $model = array();
             foreach($row as $fieldName => $fieldValue) {
-                $user[$fieldName] = $fieldValue;
+                $model[$fieldName] = $fieldValue;
             }
-            $users['user'] = $user;
-            array_push($output, $users);
+            $models['user'] = $model;
+            array_push($output, $models);
         }
         return $output;
+    }
+
+    private function updateModelFromUserInput() {
+
+        $query = new HttpQueryString();
+        $queries = $query->toArray();
+        foreach($queries as $field => $value) {
+            if (property_exists( get_class($this->model), $field) ) {
+                array_push($this->model->filterFields, $field);
+                $this->model->$field['value'] = '$value';
+            }
+        }
+
+        //Parse query string to get the filter fields
+        //parse fields to make sure that they are valid model fields.
+        //Parse message body to make sure that it is valid json.
+        //Parse json to get field values.
+        //parse fields to make sure that they are valid model fields.
+    }
+
+    private function isValidQueryString() {
 
     }
 
